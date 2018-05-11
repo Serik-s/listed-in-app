493ccf12af94c5dd3f8822d81be0aa654f70ef13
Serik-s <web.application.creater@gmail.com>
2018-05-12 04:52:19 +0600

    first commit

---
+++ Pods/Target Support Files/Pods-listed-in-app/Pods-listed-in-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_listed_in_app : NSObject
+@end
+@implementation PodsDummy_Pods_listed_in_app
+@end
---
+++ listed-in-app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  listed-in-app
+//
+//  Created by Serik Seidigalimov on 12.05.2018.
+//  Copyright © 2018 Serik Seidigalimov. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
